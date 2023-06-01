2bd6e49edbdc6b2266a9eee5b495ca4f715a71d2
Angelina Butenko <angelinaa2712@gmail.com>
2023-06-01 13:30:58 +0300

    changes

---
+++ AspNet_Configuration/EmployeeService.cs
@@ -0,0 +1,16 @@
+﻿namespace AspNet_Configuration_task1
+{
+    public interface IEmployeeService
+    {
+       string GetCompanyWithMoreEmployees(string companyName, int employeers);
+    }
+
+    public class EmployeeService : IEmployeeService
+    {
+        public string GetCompanyWithMoreEmployees(string companyName, int employeers)
+        {
+           
+            return "";
+        }
+    }
+}
--- AspNet_Configuration/Startup.cs
+++ AspNet_Configuration/Startup.cs
@@ -1,33 +1,98 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration.Ini;
+using Microsoft.Extensions.Configuration.Json;
+using Microsoft.Extensions.Configuration.Xml;
 
 namespace AspNet_Configuration
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
-        public void ConfigureServices(IServiceCollection services)
-        {
-        }
+        public IConfiguration AppConfig { get; set; }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+            var builder = new ConfigurationBuilder();
+
+            builder.SetBasePath(env.ContentRootPath);
+
+            builder.AddIniFile("IniConfig.ini");
+            builder.AddJsonFile("JsonConfig.json");
+            builder.AddXmlFile("XmlConfig.xml");
+
+            AppConfig = builder.Build();
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
 
-            app.Run(async (context) =>
+            app.Run(async (context) => 
             {
-                await context.Response.WriteAsync("Hello World!");
+                var nameFirstCompanyIni = AppConfig["NameFirstCompanyIni"];
+                var numberOfEmployeesFirstCompanyIni = AppConfig["NumberOfEmployeesFirstCompanyIni"];
+
+                var nameSecondCompanyIni = AppConfig["NameSecondCompanyIni"];
+                var numberOfEmployeesSecondCompanyIni = AppConfig["NumberOfEmployeesSecondCompanyIni"];
+
+                var nameThirdCompanyIni = AppConfig["NameThirdCompanyIni"];
+                var numberOfEmployeesThirdCompanyIni = AppConfig["NumberOfEmployeesThirdCompanyIni"];
+
+                await context.Response.WriteAsync($"First company Ini: {nameFirstCompanyIni} - employeers: {numberOfEmployeesFirstCompanyIni}, " +
+                    $"Second company ini: {nameSecondCompanyIni} - employeers: {numberOfEmployeesSecondCompanyIni}, " +
+                    $"Third company ini: {nameThirdCompanyIni} - employeers: {numberOfEmployeesThirdCompanyIni} ");
+
+                var section1 = AppConfig.GetValue<int>("NumberOfEmployeesFirstCompanyIni");
+                var section2 = AppConfig.GetValue<int>("NumberOfEmployeesSecondCompanyIni");
+                var section3 = AppConfig.GetValue<int>("NumberOfEmployeesThirdCompanyIni");
+
+                if (section1 > section2 && section1 > section3)
+                {
+                    await context.Response.WriteAsync($" More employeers Ini: {nameFirstCompanyIni}");
+                }
+                if (section2 > section3 && section2 > section1)
+                {
+                    await context.Response.WriteAsync($" More employeers Ini: {nameSecondCompanyIni} ");
+                }
+                else
+                {
+                    await context.Response.WriteAsync($" More employeers Ini: {nameThirdCompanyIni} ");
+                }
+
+
+                var nameFirstCompanyJson = AppConfig["NameFirstCompanyJson"];
+                var numberOfEmployeesFirstCompanyJson = AppConfig["NumberOfEmployeesFirstCompanyJson"];
+               
+                var nameSecondCompanyJson = AppConfig["NameSecondCompanyJson"];
+                var numberOfEmployeesSecondCompanyJson = AppConfig["NumberOfEmployeesSecondCompanyJson"];
+
+                var nameThirdCompanyJson = AppConfig["NameThirdCompanyJson"];
+                var numberOfEmployeesThirdCompanyJson = AppConfig["NumberOfEmployeesThirdCompanyJson"];
+
+                await context.Response.WriteAsync($"First company Json: {nameFirstCompanyJson} - employeers: {numberOfEmployeesFirstCompanyJson}, " +
+                    $"Second company json: {nameSecondCompanyJson} - employeers: {numberOfEmployeesSecondCompanyJson}, " +
+                    $"Third company json: {nameThirdCompanyJson} - employeers: {numberOfEmployeesThirdCompanyJson} ");
+
+
+                var nameFirstCompanyXml = AppConfig["NameFirstCompanyXml"];
+                var numberOfEmployeesFirstCompanyXml = AppConfig["NumberOfEmployeesFirstCompanyXml"];
+
+                var nameSecondCompanyXml = AppConfig["NameSecondCompanyXml"];
+                var numberOfEmployeesSecondCompanyXml = AppConfig["NumberOfEmployeesSecondCompanyXml"];
+
+                var nameThirdCompanyXml = AppConfig["NameFirstCompanyXml"];
+                var numberOfEmployeesThirdCompanyXml = AppConfig["NumberOfEmployeesFirstCompanyXml"];
+
+                await context.Response.WriteAsync($"First company Xml: {nameFirstCompanyXml} - employeers: {numberOfEmployeesFirstCompanyXml}, " +
+                    $"Second company json: {nameSecondCompanyXml} - employeers: {numberOfEmployeesSecondCompanyXml}, " +
+                    $"Third company json: {nameThirdCompanyXml} - employeers: {numberOfEmployeesThirdCompanyXml} ");
             });
         }
     }
